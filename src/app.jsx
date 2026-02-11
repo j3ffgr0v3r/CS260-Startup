40cf0ba5ecaa57d1cd9510de99f52b9b34dc3477
Jeffrey Grover <javagroover@gmail.com>
2026-02-10 21:06:53 -0700

    Routing setup

--- src/app.jsx
+++ src/app.jsx
@@ -2,36 +2,56 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { Friends } from './friends/friends';
+import { FriendSchedule } from './friend_schedule/friend_schedule';
+import { About } from './about/about';
+
 export default function App() {
     return (
-        <div className="body bg-light text-dark">
-            <header className="bg-white">
-                <div className="top">
-                    <div className="title">
-                        <h1 className="text-primary mb-0"><img className="logo" src="images/logo.svg" alt="logo" />What's Your Schedule?</h1>
+        <BrowserRouter>
+            <div className="body bg-light text-dark">
+                <header className="bg-white">
+                    <div className="top">
+                        <div className="title">
+                            <h1 className="text-primary mb-0"><img className="logo" src="images/logo.svg" alt="logo" />What's Your Schedule?</h1>
+                        </div>
+                        <div className="profile me-3">
+                            <span>Username</span><img className="profile-symbol" src="images/profile.svg" alt="profile" />
+                        </div>
                     </div>
-                    <div className="profile me-3">
-                        <span>Username</span><img className="profile-symbol" src="images/profile.svg" alt="profile" />
-                    </div>
-                </div>
 
-                {/* <!-- Navigation elements --> */}
-                <nav className="navbar navbar-expand-lg bg-white pt-0">
-                    {/* <!-- Menu is a semantic alternative to <ul> that represents an interaction --> */}
-                    <menu className="navbar-nav mt-0">
-                        <li className="nav-item"><a className="nav-link active" href="home.html">Home</a></li>
-                        <li className="nav-item"><a className="nav-link" href="friends.html">Friends</a></li>
-                        <li className="nav-item"><a className="nav-link" href="about.html">About</a></li>
-                    </menu>
-                </nav>
-            </header>
+                    {/* <!-- Navigation elements --> */}
+                    <nav className="navbar navbar-expand-lg bg-white pt-0">
+                        {/* <!-- Menu is NavLink semantic alternative to <ul> that represents an interaction --> */}
+                        <menu className="navbar-nav mt-0">
+                            <li className="nav-item"><NavLink className="nav-link active" to="home">Home</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" to="friends">Friends</NavLink></li>
+                            <li className="nav-item"><NavLink className="nav-link" to="about">About</NavLink></li>
+                        </menu>
+                    </nav>
+                </header>
 
-            <main className="m-1">App content</main>
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/friends' element={<Friends />} />
+                    <Route path='/friend_schedule' element={<FriendSchedule />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
 
-            <footer className="border-top bg-light py-1 px-2">
-                <div>Jeff Grover</div>
-                <div><a href="https://github.com/j3ffgr0v3r/CS260-Startup">GitHub</a></div>
-            </footer>
-        </div>
+                <footer className="border-top bg-light py-1 px-2">
+                    <div>Jeff Grover</div>
+                    <div><a href="https://github.com/j3ffgr0v3r/CS260-Startup">GitHub</a></div>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
