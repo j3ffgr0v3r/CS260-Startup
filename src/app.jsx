83964734eeb2e73ea0a0218079399991d4f74381
Jeffrey Grover <javagroover@gmail.com>
2026-02-10 22:52:02 -0700

    Header only applied to pages that need it

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Outlet, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Home } from './home/home';
 import { Friends } from './friends/friends';
@@ -13,31 +13,14 @@ export default function App() {
     return (
         <BrowserRouter>
             <div className="body">
-                <header className="bg-white">
-                    <div className="top">
-                        <div className="title">
-                            <h1 className="text-primary mb-0"><img className="logo" src="images/logo.svg" alt="logo" />What's Your Schedule?</h1>
-                        </div>
-                        <div className="profile me-3">
-                            <span>Username</span><img className="profile-symbol" src="images/profile.svg" alt="profile" />
-                        </div>
-                    </div>
-
-                    <nav className="navbar navbar-expand-lg bg-white pt-0">
-                        <menu className="navbar-nav mt-0">
-                            <li className="nav-item"><NavLink className="nav-link" to="home">Home</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link" to="friends">Friends</NavLink></li>
-                            <li className="nav-item"><NavLink className="nav-link" to="about">About</NavLink></li>
-                        </menu>
-                    </nav>
-                </header>
-
                 <Routes>
                     <Route path='/' element={<Login />} exact />
-                    <Route path='/home' element={<Home />} />
-                    <Route path='/friends' element={<Friends />} />
-                    <Route path='/friend_schedule' element={<FriendSchedule />} />
-                    <Route path='/about' element={<About />} />
+                    <Route element={<Header />}>
+                        <Route path='/home' element={<Home />} />
+                        <Route path='/friends' element={<Friends />} />
+                        <Route path='/friend_schedule' element={<FriendSchedule />} />
+                        <Route path='/about' element={<About />} />
+                    </Route>
                     <Route path='*' element={<NotFound />} />
                 </Routes>
 
@@ -50,6 +33,32 @@ export default function App() {
     );
 }
 
+function Header() {
+    return (
+        <>
+            <header className="bg-white">
+                <div className="top">
+                    <div className="title">
+                        <h1 className="text-primary mb-0"><img className="logo" src="images/logo.svg" alt="logo" />What's Your Schedule?</h1>
+                    </div>
+                    <div className="profile me-3">
+                        <span>Username</span><img className="profile-symbol" src="images/profile.svg" alt="profile" />
+                    </div>
+                </div>
+
+                <nav className="navbar navbar-expand-lg bg-white pt-0">
+                    <menu className="navbar-nav mt-0">
+                        <li className="nav-item"><NavLink className="nav-link" to="home">Home</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to="friends">Friends</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to="about">About</NavLink></li>
+                    </menu>
+                </nav>
+            </header>
+            <Outlet />
+        </>
+    );
+}
+
 function NotFound() {
-  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
