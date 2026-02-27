b737bcbc76ba2a55308f09259a5d3d196fc894b2
Jeffrey Grover <javagroover@gmail.com>
2026-02-26 21:46:03 -0700

    Moved calendar to a separate component

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import "./home.css";
+import "../components/calendar.jsx"
+import { Calendar } from '../components/calendar';
 
 export function Home({ username }) {
   const [events, setEvents] = React.useState([]);
@@ -17,50 +19,7 @@ export function Home({ username }) {
       <h2 className="mt-4 mb-3">What's your schedule looking like today, <i>{username}</i>?</h2>
 
       <div className="home-center">
-        <div className="mx-1 bg-white border rounded-4 calendar home-calendar">
-          <div className="day-label day-left">SUN</div>
-          <div className="day-label">MON</div>
-          <div className="day-label">TUE</div>
-          <div className="day-label">WED</div>
-          <div className="day-label">THU</div>
-          <div className="day-label">FRI</div>
-          <div className="day-label day-right">SAT</div>
-          <div className="day day-left day-top">1</div>
-          <div className="day day-top">2</div>
-          <div className="day day-top">3</div>
-          <div className="day day-top">4</div>
-          <div className="day day-top">5</div>
-          <div className="day day-top">6</div>
-          <div className="day day-top day-right">7<br /><span className="event event-timed">8:00pm LoTR Marathon</span></div>
-          <div className="day day-left">8</div>
-          <div className="day">9</div>
-          <div className="day">10</div>
-          <div className="day">11</div>
-          <div className="day">12</div>
-          <div className="day">13</div>
-          <div className="day day-right">14</div>
-          <div className="day day-left">15</div>
-          <div className="day">16</div>
-          <div className="day">17</div>
-          <div className="day">18<br /><span className="event event-all-day">Camping</span></div>
-          <div className="day">19</div>
-          <div className="day">20</div>
-          <div className="day day-right">21</div>
-          <div className="day day-left">22</div>
-          <div className="day">23<br /><span className="event event-timed">10:00am Service Project</span></div>
-          <div className="day">24</div>
-          <div className="day">25</div>
-          <div className="day">26</div>
-          <div className="day">27</div>
-          <div className="day day-right">28</div>
-          <div className="day day-bottom day-left">29</div>
-          <div className="day day-bottom">30</div>
-          <div className="day day-bottom">31</div>
-          <div className="day day-bottom">1</div>
-          <div className="day day-bottom">2</div>
-          <div className="day day-bottom">3</div>
-          <div className="day day-bottom day-right">4</div>
-        </div>
+        <Calendar />
         <div className="management home-management">
           <div className="modal fade" id="eventCreationModal" tabIndex="-1" aria-labelledby="eventCreationModalLabel" style={{display: "none"}} aria-hidden="true">
             <div className="modal-dialog">
