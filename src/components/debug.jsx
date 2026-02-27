db8391199dbc27d9db4e3e79e40a0b03d5b8b9da
Jeffrey Grover <javagroover@gmail.com>
2026-02-27 00:21:48 -0700

    Added more userEvents

--- src/components/debug.jsx
+++ src/components/debug.jsx
@@ -1,8 +1,19 @@
 export function ResetDatabase() {
     const events = [
         {
-            date: new Date(2026, 1, 7, 20),
-            title: "LoTR Marathon"
+            date: new Date(2026, 1, 6, 20),
+            title: "LoTR Marathon",
+            allDay: false
+        },
+        {
+            date: new Date(2026, 1, 17),
+            title: "Camping",
+            allDay: true
+        },
+        {
+            date: new Date(2026, 1, 22, 10),
+            title: "Service Project",
+            allDay: false
         }
     ]
     
