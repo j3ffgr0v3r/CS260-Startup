178a4691ccf2325bfb45a75e0b51f91a1ebb6816
Jeffrey Grover <javagroover@gmail.com>
2026-02-27 20:12:41 -0700

    Moved remove friend button to dropdown

--- src/friend_schedule/friend_schedule.jsx
+++ src/friend_schedule/friend_schedule.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import "./friend_schedule.css";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Calendar } from '../components/calendar';
 import { NotFound } from '../app';
+import { Dropdown } from 'react-bootstrap';
+import { useToast } from '../components/toast';
 
 export function FriendSchedule() {
   // Import and hook friends list
@@ -15,23 +17,40 @@ export function FriendSchedule() {
     localStorage.setItem('friends', JSON.stringify(friends));
   }, [friends]);
 
-
   const { friendID } = useParams();
 
-  const friend = friends.find((friend) => friend.user.username === friendID)
+  const { showToast } = useToast();
+  const navigate = useNavigate();
 
+  function removeFriend() {
+    navigate("/friends");
+    showToast({
+      title: 'Friend Removed',
+      message: `You and ${friend.user.firstName} are no longer friends`,
+      bg: 'danger',
+    });
+    setFriends((prev) => prev.filter((f) => f.user.username !== friendID));
+  }
+
+  const friend = friends.find((friend) => friend.user.username === friendID)
   if (friend == undefined) {
-    return NotFound();
+    return <NotFound />;
   }
 
   return (
     <main>
-      <h2>{friend.user.firstName}'{friend.user.firstName.at(-1) == "s" ? "" : "s"} Schedule</h2>
+      <div className="management friend-schedule-management">
+        <h2>{friend.user.firstName}'{friend.user.firstName.at(-1) == "s" ? "" : "s"} Schedule</h2>
+        <Dropdown align="end">
+          <Dropdown.Toggle id="profile-menu" />
+
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={removeFriend}><span>✖ </span>Remove Friend</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
       <div className="center">
         <Calendar year={2026} month={1} events={friend.user.events} />
-        <div className="management friend-schedule-management">
-          <button className="btn btn-danger btn-lg"><span>✖</span> Remove Friend</button>
-        </div>
       </div>
     </main>
   );
