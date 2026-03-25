b8353d6e86be4af102ece6e6a52420c843060f92
Jeffrey Grover <javagroover@gmail.com>
2026-03-24 23:39:25 -0600

    Reformat app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -18,17 +18,19 @@ export default function App() {
 
     // Get active user
     const [activeUser, setActiveUser] = React.useState(null);
-    React.useEffect(() => {const fetchUser = async () => {
-        if (authState !== AuthState.Authenticated) return;
-        const response = await fetch(`/api/users/${localStorage.getItem('username')}`);
-        if (response.status === 401) {
-            localStorage.removeItem("username");
-            setAuthState(AuthState.Unauthenticated);
-        } else {
-            response.json().then((user) => {
+    React.useEffect(() => {
+        const fetchUser = async () => {
+            if (authState !== AuthState.Authenticated) return;
+            const response = await fetch(`/api/users/${localStorage.getItem('username')}`);
+            if (response.status === 401) {
+                localStorage.removeItem("username");
+                setAuthState(AuthState.Unauthenticated);
+            } else {
+                response.json().then((user) => {
                     setActiveUser(user);
                 });
-        }};
+            }
+        };
         fetchUser();
 
     }, [authState]);
